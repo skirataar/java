fe64ec91e6093c5aabe30b66a674b41f63817fd5
skirataar <95690923+skirataar@users.noreply.github.com>
2023-02-09 11:27:28 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,1 +1,39 @@
+package com.array.in;
+import java.util.*;
+public class Demo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Alist obj = new Alist();
+		int op;
+		ArrayList<String> list = new ArrayList<String>();
+		while(true)
+		{
+			System.out.println("Enter the option");
+			System.out.println("1: append\n2: insert\n3: search\n4: show the elements which of specific letter at starting\n");
+			op= sc.nextInt();
+			switch(op)
+			{
+				case 1:
+					obj.append(list);
+					break;
+				case 2:
+					obj.insert(list);
+					break;
+				case 3:
+					obj.search(list);
+					break;
+				case 4:
+					obj.find(list);
+					break;
+				default:
+					System.out.println("Displaying the elements of ArrayList");
+					for(String s : list)
+						System.out.println(s);
+					sc.close();
+					System.exit(0);
+			}
+		}
 
+	}
+
+}
